2f0793cf902317dbe51f44258bc9419e58d73a8f
sreekanth mandala <sreekanthmandalaa@gmail.com>
2023-04-06 02:28:02 +0530

    added custome datepicker with calender

--- datePicker-with-Angular-Bulma-and-Moment.js/src/app/app-routing.module.ts
+++ datePicker-with-Angular-Bulma-and-Moment.js/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatePickerComponent } from './components/date-picker/date-picker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'' , component : DatePickerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- datePicker-with-Angular-Bulma-and-Moment.js/src/app/app.module.ts
+++ datePicker-with-Angular-Bulma-and-Moment.js/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatePickerComponent } from './components/date-picker/date-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatePickerComponent
   ],
   imports: [
     BrowserModule,
